8c0b657c324d3eecde7446abfe1db3346a996917
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-01-12 21:57:09 +0000

    refactor: move navigation bar into NavBar component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import { firekitAuth, firekitUser } from 'svelte-firekit';
 	import { setContext } from 'svelte';
 	import { localStore } from '$lib/localstore.svelte';
+	import NavBar from './NavBar.svelte';
 
 	let { children } = $props();
 
@@ -33,27 +34,7 @@
 <div class="drawer h-screen">
 	<input id="drawer-toggle" type="checkbox" class="drawer-toggle" />
 	<div class="drawer-content flex flex-col">
-		<!-- Navbar -->
-		<nav class="navbar flex-none bg-base-200">
-			<div class="flex-none {firekitUser.isLoggedIn ? '' : 'hidden'}">
-				<label for="drawer-toggle" class="btn btn-ghost drawer-button btn-sm">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						class="inline-block h-6 w-6 stroke-current"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M4 6h16M4 12h16M4 18h16"
-						></path>
-					</svg>
-				</label>
-			</div>
-			<div class="mx-2 flex-1 px-2 text-xl font-bold">Giulia</div>
-		</nav>
+		<NavBar />
 
 		<!-- Main Content -->
 		{#if firekitUser.isLoggedIn}
---
+++ src/routes/NavBar.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { firekitUser } from 'svelte-firekit';
+</script>
+
+<nav class="navbar flex-none bg-base-200">
+	<div class="flex-none {firekitUser.isLoggedIn ? '' : 'hidden'}">
+		<label for="drawer-toggle" class="btn btn-ghost drawer-button btn-sm">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				class="inline-block h-6 w-6 stroke-current"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M4 6h16M4 12h16M4 18h16"
+				></path>
+			</svg>
+		</label>
+	</div>
+	<div class="mx-2 flex-1 px-2 text-xl font-bold">Giulia</div>
+</nav>
