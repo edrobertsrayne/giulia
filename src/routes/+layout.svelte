079ffc6a8daa01d98d3c42d8c23fe6979844c3d7
Ed Roberts Rayne <ed.rayne@gmail.com>
2025-01-12 22:00:13 +0000

    refactor: move login alert message to LoginAlert component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,16 +4,9 @@
 	import { setContext } from 'svelte';
 	import { localStore } from '$lib/localstore.svelte';
 	import NavBar from './NavBar.svelte';
+	import LoginAlert from './LoginAlert.svelte';
 
 	let { children } = $props();
-
-	async function handleSignIn() {
-		try {
-			await firekitAuth.signInWithGoogle();
-		} catch (error) {
-			console.error('Authentication failed:', error);
-		}
-	}
 
 	async function handleSignOut() {
 		try {
@@ -40,29 +33,7 @@
 		{#if firekitUser.isLoggedIn}
 			{@render children()}
 		{:else}
-			<div class="m-8">
-				<div role="alert " class="alert">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						class="h-6 w-6 shrink-0 stroke-info"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-						></path>
-					</svg>
-					<span>To use Giulia, you must log in with a valid Google account.</span>
-					<div>
-						<button class="btn btn-primary btn-sm" onclick={handleSignIn}
-							>Sign in with Google</button
-						>
-					</div>
-				</div>
-			</div>
+			<LoginAlert />
 		{/if}
 	</div>
 
---
+++ src/routes/LoginAlert.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { firekitAuth } from 'svelte-firekit';
+	async function handleSignIn() {
+		try {
+			await firekitAuth.signInWithGoogle();
+		} catch (error) {
+			console.error('Authentication failed:', error);
+		}
+	}
+</script>
+
+<div class="m-8">
+	<div role="alert " class="alert">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			class="h-6 w-6 shrink-0 stroke-info"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+			></path>
+		</svg>
+		<span>To use Giulia, you must log in with a valid Google account.</span>
+		<div>
+			<button class="btn btn-primary btn-sm" onclick={handleSignIn}>Sign in with Google</button>
+		</div>
+	</div>
+</div>
